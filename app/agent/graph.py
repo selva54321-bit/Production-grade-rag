565b1e3656738cb3f120196c3153a2a61934b97d
selva54321-bit <selvag401@gmail.com>
2026-09-18 14:40:31 +0530

    small change in graph

--- app/agent/graph.py
+++ app/agent/graph.py
@@ -1,9 +1,9 @@
 from langgraph.graph import StateGraph, END
 from langgraph.checkpoint.memory import MemorySaver
-from app.agents.state import AgentState
-from app.agents.nodes.planner import planner_node
-from app.agents.nodes.retriever import retrieve_node
-from app.agents.nodes.responder import generate_node
+from app.agent.state import AgentState
+from app.agent.nodes.planner import planner_node
+from app.agent.nodes.retriever import retrieve_node
+from app.agent.nodes.responder import generate_node
 
 
 # 1. Initialize the State Graph
